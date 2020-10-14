54e1a3932cd69904ccc6f3ab0ff22b6a9ea5f2a9
KarlozArturo <karlozarturojomi@gmail.com>
2020-10-14 01:36:43 -0600

    Primer intento tabla simbolos

---
+++ src/Analizadores/Simbolos.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analizadores;
+
+/**
+ *
+ * @author angelarturo
+ */
+public class Simbolos {
+    String nombre,tipo,valor;
+    int posicion;
+    
+    public Simbolos(){}
+    public Simbolos(String nombre, String tipo, String valor,int posicion){
+    this.nombre=nombre;
+    this.tipo=tipo;
+    this.posicion = posicion;
+    this.valor=valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+    
+    
+}
